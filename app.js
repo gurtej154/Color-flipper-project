48bf999dcf6ce5803887cdb560d43c949f77d6c0
gurtej <gurtej154@gmail.com>
2021-11-10 12:18:08 +1030

    add styling to color hex & simple styles

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const color = ["green", "red", "rgba(133,122,200",
-"#f15025"];
+const colors = ["green", "red", "rgba(133,122,200",
+    "#f15025"];
 const btn = document.getElementById('btn')
 const color = document.querySelector(".color")
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     // Get random number between 0 - 3 colors[3]
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
@@ -12,6 +12,6 @@ btn.addEventListener('click', function(){
     color.textContent = colors[randomNumber];
 });
 
-function getRandomNumber(){
-    return Math.floor (Math.random()*colors.length);
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
 }
