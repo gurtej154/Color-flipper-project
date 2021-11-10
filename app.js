b1ff76943fb67cf8af09e5a8723ee3b79b7d5175
gurtej <gurtej154@gmail.com>
2021-11-10 12:05:43 +1030

    add functions to hex & simple color in js files

---
+++ app.js
@@ -0,0 +1,17 @@
+const color = ["green", "red", "rgba(133,122,200",
+"#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color")
+
+btn.addEventListener('click', function(){
+    // Get random number between 0 - 3 colors[3]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor (Math.random()*colors.length);
+}
